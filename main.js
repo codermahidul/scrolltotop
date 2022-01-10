2ab156e6804e7490925b9fd6306574bf3760b7d0
Mahidul Islam <86832773+codermahidul@users.noreply.github.com>
2022-01-10 18:31:47 +0600

    Scroll to top btn by Mahidul Official

---
+++ main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 200) {
+    $('#scrollBtn').fadeIn();
+  } else {
+    $('#scrollBtn').fadeOut();
+  }
+})
+
+$('#scrollBtn').click(function(){
+  $('html, body').animate({
+    scrollTop:0,
+  }, 1000)
+})
+
+
+})
